b001489aa52b7c191d8206c4f8c1cbd6ca7970ab
Dean Shelton <dean@dragonchain.com>
2019-11-21 18:25:37 -0800

    update contract

--- dragonchain-smart-contract/src/index.js
+++ dragonchain-smart-contract/src/index.js
@@ -12,12 +12,12 @@
  * You probably want to edit the business logic there.
  */
 
-const getStdin = require('get-stdin');
-const handler = require('./contract/handler');
+const getStdin = require("get-stdin");
+const handler = require("./contract/handler");
 
 async function main() {
   const string = await getStdin(); // <-- get input from the blockchain contract invoker.
-  const input = JSON.parse(string)
+  const input = JSON.parse(string);
   try {
     const res = await handler(input); // <-- execute your smart contract.
     process.stdout.write(JSON.stringify(res)); // <-- give the output back to the invoker to modify any state.
